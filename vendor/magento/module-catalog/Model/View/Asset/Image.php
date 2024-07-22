6eb3e64d3df375feca33c4df83a2e0c6102a4c10
Dmitrii Fediuk <dfediuk@gmail.com>
2024-07-22 06:32:29 +0100

    https://github.com/justuno-com/test-2.4.7-p1/issues/1

--- vendor/magento/module-catalog/Model/View/Asset/Image.php
+++ vendor/magento/module-catalog/Model/View/Asset/Image.php
@@ -140,7 +140,16 @@ class Image implements LocalInterface
             case CatalogMediaConfig::IMAGE_OPTIMIZATION_PARAMETERS:
                 return $this->getUrlWithTransformationParameters();
             case CatalogMediaConfig::HASH:
-                return $this->context->getBaseUrl() . DIRECTORY_SEPARATOR . $this->getImageInfo();
+				# 2024-07-22 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+				# 1.1) "How to adapt `Magento\Catalog\Model\View\Asset\Image::getUrl()` to Windows in Magento ≥ 2.4.2?":
+				# https://mage2.pro/t/6411
+				# 1.2) "How to adapt `Magento\Catalog\Model\View\Asset\Image::getImageInfo()` to Windows in Magento ≥ 2.3.0?":
+				# https://mage2.pro/t/6412
+				# 2) "Adapt the website to Windows": https://github.com/27estore/site/issues/2
+				# 3) I replaced `DIRECTORY_SEPARATOR` with '/'.
+				# 4) The original code:
+				# https://github.com/magento/magento2/blob/2.4.7-p1/app/code/Magento/Catalog/Model/View/Asset/Image.php#L143
+                return $this->context->getBaseUrl() . '/' . $this->getImageInfo();
             default:
                 throw new LocalizedException(
                     __("The specified Catalog media URL format '$this->mediaFormatUrl' is not supported.")
